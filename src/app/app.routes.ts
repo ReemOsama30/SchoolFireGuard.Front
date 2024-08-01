c0db99670fde11dfa00e8e19652973ee72572059
Reem <reem.osama3052001@gmail.com>
2024-08-01 14:55:46 +0300

    done with login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './components/home/home.component';
 
 import { ReportsComponent } from './components/reports/reports.component';
 import { AddTeacherComponent } from './components/add-teacher/add-teacher.component';
+import { LoginComponent } from './components/login/login.component';
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'add', component: AddTeacherComponent },
     { path: 'report', component: ReportsComponent },
+    {path:"login",component:LoginComponent},
     { path: '', redirectTo: 'home', pathMatch: 'full' }
   ];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule,ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder,private _router:Router) {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    const { username, password } = this.loginForm.value;
+    if (username === 'asma' && password === 'asma') {
+      alert('Login successful!');
+      this._router.navigateByUrl('/report')
+    } else {
+      alert('Invalid username or password.');
+    }
+  }
+}
